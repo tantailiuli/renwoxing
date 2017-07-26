dd5c2ab7bdaa9ba51643a1c3121c8577762945ae
Lively Magic Architect <1757977307@qq.com>
2017-07-26 16:25:25 +0800

    update

---
+++ js/accountsafe.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$(".accountsafe .top").append("<p><span class=\"color\"><span></span></span><span class=\"text\"></span></p>")
+	var $val = $(".accountsafe .top input[type='hidden']").val(),
+		$span = $(".accountsafe .top p .color span"),
+		$text = $(".accountsafe .top p .text")
+		$num = 8;
+	var $per = $val/$num;
+	if($val && $per>0 && $per<0.25 || $per==0.25){
+		$span.attr("class","cor1");
+		$text.text("极低");
+	}
+	if ($val && $per>0.25 && $per<0.5 || $per==0.5) {
+		$span.attr("class","cor2");
+		$text.text("低");
+	}
+	if ($val && $per>0.5 && $per<0.75 || $per==0.75) {
+		$span.attr("class","cor3");
+		$text.text("中");
+	}
+	if ($val && $per>0.75 && $per<1 || $per==1) {
+		$span.attr("class","cor4");
+		$text.text("高");
+	}
+	$span.css({
+		"width":$per*100+"%"
+	})
+})
---
+++ js/ckeditorconfigure.js
@@ -0,0 +1,72 @@
+//
+function SelectPullDown2(box){
+	this.selectBox = document.getElementById(box);
+	this.selectIpt = document.getElementById(box).getElementsByTagName("input")[0];
+	this.selectDivBtn = document.getElementById(box).getElementsByTagName("p")[0];
+	this.selectDivList = document.getElementById(box).getElementsByTagName("div")[0];
+	this.selectSpan = document.getElementById(box).getElementsByTagName("span");
+}
+SelectPullDown2.prototype ={
+	selectBox : '',
+	selectIpt : '',
+	selectDivBtn : '',
+	selectDivList : '',
+	selectSpan : '',
+	iptEven : function(){
+		var self = this;
+		this.selectDivBtn.onclick = function(){
+			self.selectListShow();
+		}	
+		this.selectBox.onmouseleave = function(){
+			self.selectListHide();
+		}
+		return this;	
+	},
+	selectListShow: function(){
+		this.selectBox.setAttribute("class","type border");
+		this.selectDivList.style.display = "block";
+	},
+	selectListHide: function(){
+		this.selectDivList.style.display = "none";
+		this.selectBox.setAttribute("class","type");
+	},
+	selectPick : function() {
+		var span = this.selectSpan,
+		self = this;
+		function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
+		for(var i=0;i<span.length;i++){
+			span[i].onclick = function(){
+				var this_ = i;
+				self.selectDivBtn.innerHTML ="<span>" + trimStr(this.innerHTML) + "</span><i></i>";
+				self.selectIpt.value= this_;
+				self.selectListHide();
+				if (self.selectIpt.fireEvent) self.selectIpt.fireEvent('onchange');
+				else{ 
+					var evt;
+					evt = document.createEvent("HTMLEvents");  
+					evt.initEvent("change", true, true);  
+					self.selectIpt.dispatchEvent(evt);
+				};
+			}
+		}	
+		return this;
+	},
+	init : function(){
+		this.iptEven().selectPick();	
+	}
+};
+var type = new SelectPullDown2('typeselect2');
+type.init();
+//
+$(".publishingservice .item .cover .up .upload .file").change(function(){
+	$(this).parent().prev().val($(this).val());
+	$(this).parent().parent().next().find("img").attr("src",$(this).val())
+})
+//
+CKEDITOR.replace( 'TextArea1',
+    {
+        height: 400,
+        language: 'zh-cn',
+        removeDialogTabs: 'image:advanced;link:advanced'
+    }
+);
